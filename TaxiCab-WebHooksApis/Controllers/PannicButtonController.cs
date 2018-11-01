ad380387074e9845b6170b8362fe17a0a74dd41a
carlose.mellado@outlook.com <carlose.mellado@outlook.com>
2018-11-01 02:13:09 -0600

    Add Pannic Controller

---
+++ TaxiCab-WebHooksApis/Controllers/PannicButtonController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TaxiCab_WebHooksApis.Controllers
+{
+    public class PannicButtonController : ApiController
+    {
+
+
+
+    }
+}
